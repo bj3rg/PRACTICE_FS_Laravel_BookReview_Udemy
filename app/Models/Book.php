55cf9fe9d54e3f53a944c5e9f5a2bd845cad3e7c
Angelo Burog <angelokburog30@gmail.com>
2024-12-04 15:31:01 +0800

    feat(model): add HasFactory trait to Book and Review model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    //Add this line everytime you want to create a seeder
+    use HasFactory;
     public function reviews()
     {
         return $this->hasMany(Review::class);
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    //Add this line everytime you want to create a seeder
+    use HasFactory;
     //
-    public function books()
+    public function book()
     {
         return $this->belongsTo(Book::class);
     }
