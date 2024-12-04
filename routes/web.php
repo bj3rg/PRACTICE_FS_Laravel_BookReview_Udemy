4a7954155cb2bab100ab8716a47621bab6370b2d
Angelo Burog <angelokburog30@gmail.com>
2024-12-05 00:07:54 +0800

    set up homepage redirection and fallback route; included BookRoutes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
+
+Route::fallback(function () {
+    return "Error 404";
+});
+
+require __DIR__ . '/BookRoutes.php';
